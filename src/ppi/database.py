42bf13b1cf03d02562aecc504acdf54860ab6bf2
Phuong Thanh Nguyen <s43pnguy@uni-bonn.de>
2023-11-30 21:34:34 +0100

    update homework

--- src/ppi/database.py
+++ src/ppi/database.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
+from sqlalchemy import create_engine
 
 class Database:
+    """Provides tools for working with protein interaction data and store the information in sql database"""
     def __init__(self):
         HOME = os.path.expanduser("~")
         PROJECT_FOLDER = os.path.join(HOME, ".ppi")
@@ -10,10 +12,21 @@ class Database:
         except FileExistsError:
             pass
         DB_PATH = os.path.join(PROJECT_FOLDER, "ppi.sqlite")
-        self.conn = f"sqlite:///{DB_PATH}"
+        self.conn = create_engine(f"sqlite:///{DB_PATH}")
         self.path = None
 
     def set_path_to_data_file(self,path):
+        """Set path of database for later processing
+
+        Args:
+            path (str): Path to the .tsv file
+
+        Raises:
+            FileNotFoundError: error when the path does not exist
+
+        Returns:
+            str: set path to class instance to read data file
+        """
         if os.path.isfile(path):
             self.path = path
             return self.path
@@ -21,10 +34,20 @@ class Database:
             raise FileNotFoundError
 
     def read_data(self):
+        """Generate Pandas DataFrame from .tsv file
+
+        Returns:
+            DataFrame: DataFrame from the .tsv file
+        """
         self.df = pd.read_csv(self.path,sep = "\t")
         return self.df
     
     def get_proteins(self):
+        """Generate protein DataFrame to read protein names,accession and taxid
+
+        Returns:
+            DataFrame: Protein DataFrame
+        """
         self.read_data()
         df_a = self.df[["a_uniprot_id","a_name","a_taxid"]]
         df_a = df_a.drop_duplicates(ignore_index=True)
@@ -44,6 +67,11 @@ class Database:
         return self.protein_df
     
     def get_interactions(self):
+        """Generate interaction DataFrame to read interaction information between proteins
+
+        Returns:
+            DataFrame: Interaction DataFrame with the interaction information between proteins
+        """
         self.get_proteins()
         df2 = self.df.drop(["a_name","a_taxid","b_name","b_taxid"], axis = 1)
         self.protein_df["id"] = self.protein_df.index.to_list()
@@ -59,6 +87,8 @@ class Database:
         return self.interaction
     
     def import_data(self):
+        """Generate SQL database for protein and interaction tables in a defined DB PATH
+        """
         self.get_proteins()
         self.get_interactions()
         self.protein_df.to_sql("protein", self.conn, if_exists="replace")
@@ -66,6 +96,11 @@ class Database:
     
 
     def get_table_names(self):# -> list[Any]:
+        """Get generated table names
+
+        Returns:
+            list: list of generated table names
+        """
         tables = []
         try:
             self.protein_df
@@ -80,10 +115,17 @@ class Database:
         return tables
 
     def get_columns(self,table):
-        if table == "protein":
-            return self.protein_df.columns.to_list() + ["id"]
-        elif table == "interaction":
-            return self.interaction.columns.to_list() + ["id"]
+        """Get column names from sql tabels
+
+        Args:
+            table (str): name of table
+
+        Returns:
+            list: list of column names
+        """
+        table_sql = pd.read_sql_table(table,self.conn)
+        return table_sql.columns.tolist()
+
         
 
 
