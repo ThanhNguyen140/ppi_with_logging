afecdd54ce117f9c3c7d3a5e4f4fbbb638e0e3f2
Phuong Thanh Nguyen <s43pnguy@uni-bonn.de>
2023-12-05 21:53:22 +0100

    update_week7

---
+++ src/ppi/intact_analyzer.py
@@ -0,0 +1,57 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class IntActAnalyzer:
+    def __init__(self, graph: nx.MultiGraph):
+        self.graph: nx.MultiGraph = graph
+
+    def draw_graph(self, edge_label="id", node_label="id", figsize=(10, 5)):
+        """Shows the graph.
+
+        Arguments `edge_label` and `node_label` allows to change the labels in the graph.
+
+        node_label: Any of the keys in node data
+        edge_label: Any of the keys in edge data
+
+        Args:
+            edge_label (str, optional): Label to be shown on edges. Defaults to "id".
+            node_label (str, optional): Label to be shown on nodes. Defaults to "id".
+            figsize (tuple, optional): Size of the graph. Defaults to (10, 5).
+        """
+        plt.figure(figsize=figsize)
+        if node_label == "id":
+            node_labels = {x: x for x in self.graph.nodes}
+        else:
+            node_labels = {x: self.graph.nodes[x][node_label] for x in self.graph.nodes}
+        edge_labels = {}
+        for edge in self.graph.edges:
+            eid = str(self.graph.edges[edge][edge_label])
+            if edge[:2] not in edge_labels:
+                edge_labels[edge[:2]] = eid
+            else:
+                edge_labels[edge[:2]] += f",{eid}"
+        pos = nx.spring_layout(self.graph)  # Layout for the graph
+        nx.draw_networkx_nodes(self.graph, pos)
+        nx.draw_networkx_edges(self.graph, pos)
+        nx.draw_networkx_labels(self.graph, pos, labels=node_labels)
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels)
+
+        plt.show()
+
+    def get_neighbors_name(self,name):
+        nodes = dict(self.graph.nodes(data=True))
+        for node,data in nodes.items():
+            if data["name"] == name:
+                neighbors = [n for n in self.graph.neighbors(node)]
+                name_neighbors = [self.graph.nodes[n]["name"] for n in neighbors]
+        return name_neighbors
+    
+    def get_protein_with_highest_bc(self):
+        dct = nx.betweenness_centrality(self.graph)
+        node = sorted(dct.items(), key = lambda x: x[1])[-1]
+        data = self.graph.nodes[node[0]]
+        data["node_id"] = node[0]
+        data["bc_value"] = node[1]
+        return data
+
